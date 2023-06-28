db58288aa2bb3e4116651a4d3c65cb97ee1f5887
Thorus-Slughorm <92204672+Thorus-Slughorm@users.noreply.github.com>
2023-06-28 12:31:25 +0530

    Add files via upload

---
+++ eg_concat.js
@@ -0,0 +1,32 @@
+// // let petanimals = ['dogs', 'cat', 'rabbit'];
+// // let wildanimals = ['lion', 'tiger', 'wolf'];
+
+// // let animal = petanimals.concat(wildanimals);
+
+// // console.log(animal);
+// // console.log(petanimals);
+
+
+// //includes method in array
+
+// let array = ['Tanish', 'Ayush', 'Himanshhu'];
+// let isAvailable = array.includes('Tanish');
+// console.log(isAvailable);
+
+
+// //find the index of array
+// let array = [2, 5, 4, 6, 8, 10];
+// let indexArray = array.indexOf(5);
+// console.log(indexArray);
+
+//reverse an array
+let num = [1, 2, 3, 4, 5, 6];
+num.reverse(); //it doesnt return the value it just update order of that array 
+//on what it is applied
+console.log(num);
+
+//convert an array to string
+let array = [1, 2, 3, 4, 5];
+let mystring = array.toString();
+console.log(array);
+console.log(mystring);
